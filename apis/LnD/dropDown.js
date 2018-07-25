787678ed5065b0235386b89c4587c43d2de8187d
Rajesh Adhikari <anukumar@innominds.com>
2018-07-25 23:40:29 +0530

    added contenttype dropdown

--- apis/LnD/dropDown.js
+++ apis/LnD/dropDown.js
@@ -7,23 +7,31 @@ exports.dropDown = function (req, res) {
 		types = req.query.type ? req.query.type.split(',') : '',
 		batchTable = 'enrolledBatches',
 		scoresTable = 'courseWiseScoresDistribution',
+		contentTypeTable = 'contentConsumption',
 		batchAttributes = [[models[tenant].fn('DISTINCT',models[tenant].col('batch_id')), 'id'],'name'],
-		filterAttributes = [[models[tenant].fn('DISTINCT',models[tenant].col('module_name')), 'filterName'],'questionPaperId'],
+		scoreAttributes = [[models[tenant].fn('DISTINCT',models[tenant].col('module_name')), 'filterName'],'questionPaperId'],
+		contentTypeAttributes = [[models[tenant].fn('DISTINCT',models[tenant].col('content_type')), 'id'],['content_type','name']],
 		batchOptions = {
 			req: req,
 			attributes: batchAttributes
 		},
 		scoresOptions = {
 			req: req,
-			attributes: filterAttributes
+			attributes: scoreAttributes
+		},
+		contentTypeOptions = {
+			req: req,
+			attributes: contentTypeAttributes
 		},
 		batchQuery = apis.getQuery(batchOptions),
 		scoresQuery = apis.getQuery(scoresOptions),
+		contentTypeQuery = apis.getQuery(contentTypeOptions),
 		isBatch = false,
 		isQuiz = false,
 		isAssignment = false,
+		isContentType = false,
 		responseData = [];
-
+	
 	if (types.length > 0) {
 		_.each(types, function(type) {
 			switch(type.toUpperCase()) {
@@ -32,6 +40,8 @@ exports.dropDown = function (req, res) {
 				case 'QUIZ'  : isQuiz = true;
 							   break;
 			    case 'ASSIGNMENT'  : isAssignment = true;
+							   break;
+				case 'CONTENTTYPE'  : isContentType = true;
 							   break;
 			}
 		})
@@ -49,7 +59,7 @@ exports.dropDown = function (req, res) {
 				next(null, []);
 			}
 		},
-		filters: function(next) {
+		scores: function(next) {
 			if(isQuiz || isAssignment) {
 				models[tenant+'_'+scoresTable].findAll(scoresQuery).then(function (data) {
 					var quizes = [],
@@ -74,17 +84,30 @@ exports.dropDown = function (req, res) {
 				})
 			}
 		},
+		contentType: function(next) {
+			if(isContentType){
+				models[tenant+'_'+contentTypeTable].findAll(contentTypeQuery).then(function (data) {
+					next(null, data);
+				}).catch(function (err) {
+					next(err);
+				});
+			}else{
+				next(null, []);
+			}
+		}
 	},function(err, result) {
 		if(err){
 			response.customErrorMessage(res, err.message);
 		}else{
 			var batches = result.batches,
-				quizes = result.filters.quizes,
-				assignments = result.filters.assignments;
+				quizes = result.scores.quizes,
+				assignments = result.scores.assignments,
+				contentType = result.contentType;
 
 			if(isBatch) responseData.push({type: 'batch',data: batches});
 			if(isQuiz) responseData.push({type: 'quiz',data: quizes});
 			if(isAssignment) responseData.push({type: 'assignment',data: assignments});
+			if(isContentType) responseData.push({type: 'contentType',data: contentType});
 
 			response.sendSuccessResponse(res, responseData);
 		}
