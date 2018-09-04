d286abde3b4dc0370df2323b0e8a75d344b62413
Rajesh Adhikari <anukumar@innominds.com>
2018-09-04 17:40:44 +0530

    changed organization interests apis

--- apis/LnD/organizationInterests.js
+++ apis/LnD/organizationInterests.js
@@ -19,23 +19,23 @@ exports.organizationInterests = function (req, res) {
 	if(searchBy && searchTerm){
 		monthlyFilters = monthlyFilters + ` AND df.program_name like '%${searchTerm}%'`;
 	}
-   	query = `SELECT df.user_id, df.user_type, df.course_id as courseId, df.program_id AS programId, 
-	   	     	CONCAT(df.program_short_name,'-',df.course_name) AS courseName,
-	   	     	ROUND(AVG(df.hits)) as hits,
+   	query = `SELECT df.user_id, df.user_type, df.course_id as courseId, df.program_id AS programId,
+   				df.entity_id as entityId,df.entity_name AS courseName,
+	   	     	ROUND(AVG(df.viewcount)) as hits,
 	   	     	mo.monthly_hits as hitsSinceLastMonth
 				FROM muln_organization_interests AS df
-			LEFT JOIN (SELECT user_id, user_type, course_id, program_id, 
+			LEFT JOIN (SELECT user_id, user_type, course_id, program_id, entity_id,
 							  ROUND(AVG(monthly_hits)) as monthly_hits
 				 	  FROM muln_monthly_organization_interests 
 	    			  WHERE load_date=DATE_FORMAT(DATE_SUB(NOW(), INTERVAL 1 MONTH), '%M-%Y') `
-	    				+ filters + ` GROUP BY 1,2,3,4
+	    				+ filters + ` GROUP BY 1,2,3,4,5
 			) AS mo
 			ON df.user_id=mo.user_id AND 
    			   df.user_type=mo.user_type AND 
    			   df.course_id=mo.course_id AND
    			   df.program_id=mo.program_id 
 			WHERE  df.load_date BETWEEN '${date.start}' AND '${date.end}' `+ monthlyFilters + 
-			` GROUP BY 1,2,3,4 ORDER BY df.hits DESC LIMIT 3`;
+			` GROUP BY 1,2,3,4,5 ORDER BY df.hits DESC LIMIT 3`;
 	
 	models[tenant].query(query, {type: models[tenant].QueryTypes.SELECT}).then(function (data) {
 	    response.sendSuccessResponse(res, data);			
--- apis/LnD/organizationPopularTopics.js
+++ apis/LnD/organizationPopularTopics.js
@@ -12,11 +12,11 @@ exports.organizationPopularTopics = function (req, res) {
 		query = '';
 
    	query = `SELECT user_id, user_type, course_id as courseId, program_id AS programId, 
-	   	     	CONCAT(program_short_name,'-',course_name) AS courseName,
+   					df.entity_id as entityId,df.entity_name AS courseName,
 	   	     	ROUND(avg(followers)) as noOfFollowers
 				FROM muln_organization_interests 
 			WHERE  load_date BETWEEN '${date.start}' AND '${date.end}' `+ filters + 
-			` GROUP BY 1,2,3,4 ORDER BY noOfFollowers DESC LIMIT 10`;
+			` GROUP BY 1,2,3,4,5 ORDER BY noOfFollowers DESC LIMIT 10`;
 
 	models[tenant].query(query, {type: models[tenant].QueryTypes.SELECT}).then(function (data) {
 		for(var i= 0; i< data.length;i++){
