c786c998e9dcf95c6df6f8a71431e25db5d7bd21
Rajesh Adhikari <anukumar@innominds.com>
2018-09-04 19:47:13 +0530

    changed courses dropdwon

--- apis/LnD/coursesAndProgramsDropDown.js
+++ apis/LnD/coursesAndProgramsDropDown.js
@@ -4,14 +4,15 @@ var response = require('./../../helpers/response'),
 exports.coursesAndProgramsDropDown = function (req, res) {	
 	var tenant = req.headers['tenant_name'] ? req.headers['tenant_name'] : 'MAIT',
 		userId = parseInt([req.headers['user_id']] || 0),
+		group = ' group by 1,2'
 		filters = '';
 
 	if (userId) {
 		filters = ' where user_id='+userId;	
 	}
 
-	var query = `select program_id as programId, course_id as courseId,CONCAT(program_name, '-',course_name) as courseName from muln_enrolled_persons group by 1,2,3`;
-	query = query + filters;
+	var query = `select program_id as programId, course_id as courseId,CONCAT(program_name, '-',course_name) as courseName from muln_enrolled_persons `;
+	query = query + filters + group;
 	
 	models[tenant].query(query, {type: models[tenant].QueryTypes.SELECT}).then(function (data) {
 	    response.sendSuccessResponse(res, data);
