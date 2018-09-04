b6720a36b7d0b9ab0ca2896e50634426f399b103
Rajesh Adhikari <anukumar@innominds.com>
2018-09-04 19:01:06 +0530

    changed courses dropdown table muln_enrolled_persons

--- apis/LnD/coursesAndProgramsDropDown.js
+++ apis/LnD/coursesAndProgramsDropDown.js
@@ -10,7 +10,7 @@ exports.coursesAndProgramsDropDown = function (req, res) {
 		filters = ' where user_id='+userId;	
 	}
 
-	var query = `select distinct program_id as programId, course_id as courseId,CONCAT(program_name, '-',course_name) as courseName from muln_im_enrolled_courses`;
+	var query = `select distinct program_id as programId, course_id as courseId,CONCAT(program_name, '-',course_name) as courseName from muln_enrolled_persons`;
 	query = query + filters;
 	
 	models[tenant].query(query, {type: models[tenant].QueryTypes.SELECT}).then(function (data) {
