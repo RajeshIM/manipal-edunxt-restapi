20f268cdfb770fb894323504b3f1d2350b7fc253
Rajesh Adhikari <anukumar@innominds.com>
2018-09-17 14:57:41 +0530

    added date filter in courses-dropdown

--- apis/LnD/coursesAndProgramsDropDown.js
+++ apis/LnD/coursesAndProgramsDropDown.js
@@ -1,17 +1,23 @@
 var response = require('./../../helpers/response'),
+	utils = require('./../../helpers/utils'),
 	apis = require('./../../helpers/apis');
 
 exports.coursesAndProgramsDropDown = function (req, res) {	
 	var tenant = req.headers['tenant_name'] ? req.headers['tenant_name'] : 'MAIT',
+		date = utils.getDates(req),
 		userId = parseInt([req.headers['user_id']] || 0),
+		userType = req.headers['user_type'],
 		group = ' group by 1,2',
 		filters = '';
 
 	if (userId) {
-		filters = ' where user_id='+userId;	
+		filters = ' and user_id='+userId;	
+	}else if(userType){
+		filters = filters.length > 0 ? filters + ' and user_type='+ userType : ' and user_type='+ userType;
 	}
 
-	var query = `select program_id as programId, course_id as courseId,CONCAT(program_name, '-',course_name) as courseName from muln_enrolled_persons `;
+	var query = `select program_id as programId, course_id as courseId,CONCAT(program_name, '-',course_name) as courseName from muln_enrolled_persons 
+				  where load_date between '${date.start}' and '${date.end}' `;
 	query = query + filters + group;
 	
 	models[tenant].query(query, {type: models[tenant].QueryTypes.SELECT}).then(function (data) {
