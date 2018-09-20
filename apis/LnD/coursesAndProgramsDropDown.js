55daba88a7ca1521ebab8488dff4dd3adf9797f4
Rajesh Adhikari <anukumar@innominds.com>
2018-09-20 15:05:16 +0530

    changes course-dropdown filter

--- apis/LnD/coursesAndProgramsDropDown.js
+++ apis/LnD/coursesAndProgramsDropDown.js
@@ -7,7 +7,6 @@ exports.coursesAndProgramsDropDown = function (req, res) {
 		date = utils.getDates(req),
 		userId = parseInt([req.headers['user_id']] || 0),
 		userType = req.headers['user_type'],
-		group = ' group by 1,2',
 		filters = '';
 
 	if (userId) {
@@ -16,10 +15,12 @@ exports.coursesAndProgramsDropDown = function (req, res) {
 		filters = filters.length > 0 ? filters + ' and user_type='+ userType : ' and user_type='+ userType;
 	}
 
-	var query = `select program_id as programId, course_id as courseId,CONCAT(program_name, '-',course_name) as courseName from muln_enrolled_persons 
-				  where load_date>='${date.start}' `;
-	query = query + filters + group;
-	
+	var query = `SELECT * FROM (
+					SELECT program_id AS programId, course_id AS courseId,
+						   CONCAT(program_name, '-',course_name) AS courseName FROM muln_enrolled_persons 
+						   WHERE load_date>='${date.start}' `+ filters + ` ORDER BY load_date DESC
+				 )a group by 1,2`;
+
 	models[tenant].query(query, {type: models[tenant].QueryTypes.SELECT}).then(function (data) {
 	    response.sendSuccessResponse(res, data);
 	}).catch(function (err) {
